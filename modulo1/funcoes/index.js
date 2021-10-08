1016393ca7dd808245e1d33b676eae7593ea944a
João Paulo <joaop98.junior@gmail.com>
2021-10-07 21:51:53 -0300

    Funcoes

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,25 @@
+//Exercícios de interpretação de código
+
+//1-Leia o código abaixo:
+
+/*function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+console.log(minhaFuncao(2))
+console.log(minhaFuncao(10))*/
+
+//a) O que vai ser impresso no console?
+
+//Resposta: 10 e 50
+
+/*b) O que aconteceria se retirasse os dois console.log e 
+simplesmente invocasse a função minhaFuncao(2) e minhaFuncao(10)?*/
+
+//Daria um erro, já que não reconhece "minhaFuncao" como um função
+
+
+
+
+
+
