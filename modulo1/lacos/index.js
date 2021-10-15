d70f34c307a2b667a5b094fde92c0e2c48d7f540
João Paulo <joaop98.junior@gmail.com>
2021-10-14 22:11:04 -0300

    Lacos

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,26 @@
+/*Exercícios de interpretação de código
+
+//1- O que o código abaixo está fazendo? Qual o resultado impresso no console?
+
+let valor = 0
+for(let i = 0; i < 5; i++) {
+  valor += i
+}
+console.log(valor)
+
+Resposta : 10
+
+2- Leia o código abaixo:
+
+const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+  if (numero > 18) {
+		console.log(numero)
+	}
+}
+
+a) O que vai ser impresso no console?
+
+19,21,23,25,27,30 */
+
+  
