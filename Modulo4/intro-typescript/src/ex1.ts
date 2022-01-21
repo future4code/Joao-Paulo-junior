0e5f202d79f47aeb386e43a23372814125981763
João Paulo <joaop98.junior@gmail.com>
2022-01-21 18:59:32 -0300

    Atividade de quarta-feira

---
+++ Modulo4/intro-typescript/src/ex1.ts
@@ -0,0 +1,23 @@
+/*A seguinte função em JavaScript recebe como parâmetro os tamanhos dos três lados do triângulo: 
+ladoA, ladoB, ladoC e retorna se ele é equilátero, isósceles ou escaleno. 
+Refatore a função para o Typescript.*/
+
+
+function checaTriangulo(a, b, c) {
+
+    const a:string = process.argv[2]
+    const b:string = process.argv[3]
+    const a:string = process.argv[2]
+
+
+
+    if (a !== b && b !== c) {
+      return "Escaleno";
+    } else if (a === b && b === c) {
+      return "Equilátero";
+    } else {
+      return "Isósceles";
+    }
+  }
+
+  
